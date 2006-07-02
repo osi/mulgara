1637e8ae5c513a1c9dd2e32c3d50e545c779bde6
brian <brian@1b8e2379-6217-0410-9814-82e2034ecad4>
2006-07-02 17:03:41 +0000

    More Kowari->Mulgara changes.
    git-svn-id: http://mulgara.org/svn/mulgara/trunk@21 1b8e2379-6217-0410-9814-82e2034ecad4

--- src/jar/demo-mp3/java/org/mulgara/demo/mp3/Mp3ConfigFile.java
+++ src/jar/demo-mp3/java/org/mulgara/demo/mp3/Mp3ConfigFile.java
@@ -40,11 +40,11 @@ import org.jrdf.graph.URIReference;
 
 // Local packages
 import org.mulgara.query.rdf.*;
-import org.mulgara.rdql.demo.mp3.config.FileSystemModel;
-import org.mulgara.rdql.demo.mp3.config.Mp3ConfigXML;
-import org.mulgara.rdql.demo.mp3.config.Mp3Model;
-import org.mulgara.rdql.demo.mp3.config.SchemaFile;
-import org.mulgara.rdql.demo.mp3.config.SchemaModel;
+import org.mulgara.demo.mp3.config.FileSystemModel;
+import org.mulgara.demo.mp3.config.Mp3ConfigXML;
+import org.mulgara.demo.mp3.config.Mp3Model;
+import org.mulgara.demo.mp3.config.SchemaFile;
+import org.mulgara.demo.mp3.config.SchemaModel;
 
 /**
  * Mp3Config implementation that can be loaded and persisted as an XML File.
