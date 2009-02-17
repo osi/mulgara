osi/mulgara

commit ff5718c9d9dbc8fdc05d4ea567424f81989086fa

Added support for exporting to NTriples. If the filename ends in .n3, .nt, or .ttl then the output will be in NTriples, otherwise RDF/XML is used.
git-svn-id: http://mulgara.org/svn/mulgara/trunk@1507 1b8e2379-6217-0410-9814-82e2034ecad4

src/jar/resolver/java/org/mulgara/resolver/ExportOperation.java
@@ -17,6 +17,7 @@ import java.net.URI;
 import java.util.Map;
 
 import org.mulgara.content.rdfxml.writer.RDFXMLWriter;
+import org.mulgara.content.n3.N3Writer;
 import org.mulgara.query.Constraint;
 import org.mulgara.query.ConstraintImpl;
 import org.mulgara.query.LocalNode;
@@ -103,9 +104,14 @@ class ExportOperation extends OutputOperation {
 
         // Do the writing.
         try {
-          // TODO: Use the destination URI file suffix to determine the appropriate writer.
-          RDFXMLWriter rdfWriter = new RDFXMLWriter();
-          rdfWriter.write(graphStatements, systemResolver, writer, prefixes);
+          String path = destinationURI.getPath();
+          if (path.endsWith(".n3") || path.endsWith(".nt") || path.endsWith(".ttl")) {
+            N3Writer n3Writer = new N3Writer();
+            n3Writer.write(graphStatements, systemResolver, writer);
+          } else {
+            RDFXMLWriter rdfWriter = new RDFXMLWriter();
+            rdfWriter.write(graphStatements, systemResolver, writer, prefixes);
+          }
         } finally {
           // This will close the wrapped resolution as well.
           graphStatements.close();

src/jar/resolver/build.xml
@@ -32,6 +32,7 @@
     <fileset file="${resolver-jar.dist.dir}/${resolver-jar.jar}"/>
     <fileset file="${resolver-url.dist.dir}/${resolver-url.jar}"/>
     <fileset file="${content-rdfxml.dist.dir}/${content-rdfxml.jar}"/>
+    <fileset file="${content-n3.dist.dir}/${content-n3.jar}"/>
     <fileset file="${driver.dist.dir}/${driver.jar}"/>
   </path>
 
@@ -81,7 +82,7 @@
   <target name="resolver-compile"
           depends="-resolver-prepare, resolver-spi-jar, rules-jar,
                    resolver-http-jar, resolver-file-jar, resolver-url-jar,
-                   resolver-jar-jar, content-rdfxml-jar, content-rdfxml-jar,
+                   resolver-jar-jar, content-rdfxml-jar, content-n3-jar,
                    client-jrdf-jar, jrdf-jar, resolver-xsd-jar, driver-jar"
           description="Compiles all resolver related files included generated
                        source code">

commit 37e82fc1056c59c0435678cf79e503e79f7c5173

Fixed NPE when no destination-uri is specified (bug introduced in r1507).
git-svn-id: http://mulgara.org/svn/mulgara/trunk@1509 1b8e2379-6217-0410-9814-82e2034ecad4

src/jar/resolver/java/org/mulgara/resolver/ExportOperation.java
@@ -104,8 +104,8 @@ class ExportOperation extends OutputOperation {
 
         // Do the writing.
         try {
-          String path = destinationURI.getPath();
-          if (path.endsWith(".n3") || path.endsWith(".nt") || path.endsWith(".ttl")) {
+          String path = (destinationURI != null) ? destinationURI.getPath() : null;
+          if (path != null && (path.endsWith(".n3") || path.endsWith(".nt") || path.endsWith(".ttl"))) {
             N3Writer n3Writer = new N3Writer();
             n3Writer.write(graphStatements, systemResolver, writer);
           } else {