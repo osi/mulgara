ad672b132935907ef9f929dfa1b61a9397659666
alexhall <alexhall@1b8e2379-6217-0410-9814-82e2034ecad4>
2009-10-20 12:26:21 +0000

    Relicensing files to Apache 2.0
    git-svn-id: http://mulgara.org/svn/mulgara/trunk@1825 1b8e2379-6217-0410-9814-82e2034ecad4

--- src/jar/query/java/org/mulgara/connection/ConnectionFactoryUnitTest.java
+++ src/jar/query/java/org/mulgara/connection/ConnectionFactoryUnitTest.java
@@ -1,13 +1,17 @@
 /*
- * The contents of this file are subject to the Open Software License
- * Version 3.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.opensource.org/licenses/osl-3.0.txt
+ * Copyright 2009 Revelytix.
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
- * the License for the specific language governing rights and limitations
- * under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mulgara.connection;
 
